13f9577564c056e40340360d97debab0d4bfe7a4
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2017-08-04 12:50:01 +0900

    ストアをnewしてプロバイダーに入れた

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Provider } from 'mobx-react/native';
 import Todo from './components';
+import TodoStore from './stores/todo';
+
+const stores = {
+  todo: new TodoStore(),
+};
 
 export default () => (
-  <Todo />
+  <Provider {...stores}>
+    <Todo />
+  </Provider>
 );
